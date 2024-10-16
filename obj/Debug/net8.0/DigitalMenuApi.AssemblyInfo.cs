5ac340464eff06183002ea198aec9fceabfc6468
devMotcho <code.mmd2@gmail.com>
2024-10-16 13:36:05 +0100

    added gitignore

--- obj/Debug/net8.0/DigitalMenuApi.AssemblyInfo.cs
+++ obj/Debug/net8.0/DigitalMenuApi.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DigitalMenuApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+27e20ee37f0b3300e0775d16b7e280b3b323899d")]
 [assembly: System.Reflection.AssemblyProductAttribute("DigitalMenuApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DigitalMenuApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
